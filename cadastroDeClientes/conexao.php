4f8e4999cc6f6a9c236e45d980f246677de9f109
Weber de Oliveira Rocha <76965306+Jamallc@users.noreply.github.com>
2021-01-11 20:46:45 -0300

    Delete cadastroDeClientes directory

--- cadastroDeClientes/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('HOST', 'localhost');
-define('USUARIO', 'banco');
-define('SENHA', 'banco');
-define('DB', 'cadastrocliente');
-
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar!');
-
-?>
